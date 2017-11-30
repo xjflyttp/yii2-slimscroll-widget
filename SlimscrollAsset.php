79f8541db5243f92ed08e9c4a64fb366eb664a18
xjflyttp <xjflyttp@gmail.com>
2017-11-30 11:30:54 +0800

    fix basePath

--- SlimscrollAsset.php
+++ SlimscrollAsset.php
@@ -7,7 +7,6 @@ use yii\web\AssetBundle;
 class SlimscrollAsset extends AssetBundle {
 
     public $sourcePath = '@bower/slimscroll';
-    public $basePath = '@webroot/assets';
     public $js = ['jquery.slimscroll.min.js'];
     public $depends = ['yii\web\JqueryAsset'];
 
