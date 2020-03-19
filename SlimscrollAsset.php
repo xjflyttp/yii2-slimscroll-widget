0d6734065418de6ff0f8b6fd66b772f33527cf94
xjflyttp <xjflyttp@gmail.com>
2020-03-19 14:31:16 +0800

    update bower-asset

--- SlimscrollAsset.php
+++ SlimscrollAsset.php
@@ -6,8 +6,8 @@ use yii\web\AssetBundle;
 
 class SlimscrollAsset extends AssetBundle {
 
-    public $sourcePath = '@bower/slimscroll';
+    public $sourcePath = '@bower/jquery-slimscroll';
     public $js = ['jquery.slimscroll.min.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
+	public $publishOptions = ['only' => ['jquery.slimscroll.min.js']];
 }
